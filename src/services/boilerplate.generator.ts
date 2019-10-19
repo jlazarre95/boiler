67992b638dc49910f0b4838965a2f664391e3f12
Jahkell Lazarre <jlaza020@fiu.edu>
2019-10-18 23:57:12 -0400

    feat: fix outDir; run additional template file scripts; add winston logger

--- src/services/boilerplate.generator.ts
+++ src/services/boilerplate.generator.ts
@@ -1,4 +1,5 @@
 import { ConsoleColors } from "@app/console/console-colors";
+import { BoilerConstants } from "@app/constants";
 import { ScriptRunner } from "@app/services/script.runner";
 import { IBoilerplateContext } from "@app/types/boilerplate-context.interface";
 import { Dict } from "@app/types/dict.type";
@@ -6,20 +7,22 @@ import { PackageConfig, PackageConfigTemplate, PackageConfigTemplateInclude } fr
 import { evalString, evalUrl } from "@app/utils/boilerplate.utils";
 import { getPackageConfigPath, getScriptPath, getTemplatePath } from "@app/utils/directory.utils";
 import * as fs from "fs-extra";
-import { dirname, join } from "path";
+import { basename, dirname, join } from "path";
+import { Logger } from "winston";
 import { ParamResolver } from "./param.resolver";
 
 export class BoilerplateGenerator {
 
-    constructor(private paramResolver: ParamResolver, private scriptRunner: ScriptRunner) { }
+    constructor(private paramResolver: ParamResolver, private scriptRunner: ScriptRunner, private logger: Logger) { }
 
     async generateBoilerplate(projectPath: string, boilerplatePath: string, packageName: string, templateName: string, args: string[]) {
+        templateName = basename(templateName, BoilerConstants.TEMPLATE_EXT);
         const config: PackageConfig = PackageConfig.create(await fs.readJSON(getPackageConfigPath(boilerplatePath, packageName)));
         const params: Dict<string, string> = await this.paramResolver.resolveParams(projectPath, boilerplatePath, packageName, templateName, config, args);
         const context: IBoilerplateContext = { params: params, outDir: "" };
-        await this.scriptRunner.runScript(projectPath, getScriptPath(boilerplatePath, packageName, `before.js`), context);
+        await this.scriptRunner.runScript(projectPath, getScriptPath(boilerplatePath, packageName, `before`), { params: params, outDir: "" });
         await this.generateBoilerplateTemplate(projectPath, boilerplatePath, packageName, templateName, config, context);
-        await this.scriptRunner.runScript(projectPath, getScriptPath(boilerplatePath, packageName, `after.js`), context);
+        await this.scriptRunner.runScript(projectPath, getScriptPath(boilerplatePath, packageName, `after`), { params: params, outDir: "" });
     }
     
     private async generateBoilerplateTemplate(projectPath: string, boilerplatePath: string, packageName: string, templateName: string, 
@@ -31,8 +34,9 @@ export class BoilerplateGenerator {
         
         // TODO remove
         if(!template.outDir) template.outDir = "";
-    
-        await this.scriptRunner.runScript(projectPath, getScriptPath(boilerplatePath, packageName, `before-${templateName}.js`), context);
+        
+        context.outDir = join(context.outDir, template.outDir);
+        await this.scriptRunner.runScript(projectPath, getScriptPath(boilerplatePath, packageName, `before-${templateName}`), context);
     
         // Convert included templates to an array.
         let includes: (string | PackageConfigTemplateInclude)[];
@@ -50,11 +54,11 @@ export class BoilerplateGenerator {
             if(!include.outDir) include.outDir = "";
             
             const newContext: IBoilerplateContext = { 
-                outDir: join(context.outDir, template.outDir, include.outDir), 
+                outDir: join(context.outDir, include.outDir), 
                 params: context.params
             };
     
-            const templatePath: string = await getTemplatePath(boilerplatePath, packageName, include.name);
+            const templatePath: string = getTemplatePath(boilerplatePath, packageName, include.name);
             if(await fs.pathExists(templatePath)) {
                 // Template file.
                 await this.generateBoilerplateFile(projectPath, boilerplatePath, packageName, include.name, config, newContext);
@@ -64,14 +68,17 @@ export class BoilerplateGenerator {
             }
         }
     
-        await this.scriptRunner.runScript(projectPath, getScriptPath(projectPath, packageName, `after-${templateName}.js`), context);
+        await this.scriptRunner.runScript(projectPath, getScriptPath(projectPath, packageName, `after-${templateName}`), context);
     }
     
     private async generateBoilerplateFile(projectPath: string, boilerplatePath: string, packageName: string, 
                                           templateName: string, config: PackageConfig, context: IBoilerplateContext) {
+        templateName = basename(templateName, BoilerConstants.TEMPLATE_EXT);
+
         // Run before script.
-        await this.scriptRunner.runScript(projectPath, getScriptPath(boilerplatePath, packageName, `before-${templateName}.js`), context);
-    
+        await this.scriptRunner.runScript(projectPath, getScriptPath(boilerplatePath, packageName, `before-${templateName}`), context);
+        await this.scriptRunner.runScript(projectPath, getScriptPath(boilerplatePath, packageName, `before-${templateName}${BoilerConstants.TEMPLATE_EXT}`), context);
+
         // Load template.
         const templatePath: string = await getTemplatePath(boilerplatePath, packageName, templateName);
         const template: string = (await fs.readFile(templatePath)).toString();
@@ -81,15 +88,16 @@ export class BoilerplateGenerator {
     
         // Save boilerplate.
         // TODO use --force to replace existing files.
-        const destPath: string = join(context.outDir, templateName);
+        const destPath: string = join(projectPath, context.outDir, templateName);
         const modifiedDestPath = evalUrl(destPath, config, context.params);
         await fs.mkdirp(dirname(modifiedDestPath));
         await fs.writeFile(modifiedDestPath, boilerplate);
     
         // Run after script.
-        await this.scriptRunner.runScript(projectPath, getScriptPath(boilerplatePath, packageName, `after-${templateName}.js`), context);
-    
-        console.log(`${ConsoleColors.Green}+${ConsoleColors.Reset} ${modifiedDestPath}`);
+        await this.scriptRunner.runScript(projectPath, getScriptPath(boilerplatePath, packageName, `after-${templateName}`), context);
+        await this.scriptRunner.runScript(projectPath, getScriptPath(boilerplatePath, packageName, `after-${templateName}${BoilerConstants.TEMPLATE_EXT}`), context);
+
+        this.logger.info(`${ConsoleColors.Green}+${ConsoleColors.Reset} ${modifiedDestPath}`);
     }
 
 }
