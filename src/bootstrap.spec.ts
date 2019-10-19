28a6c864b367fa861993c011045c258550f04854
Jahkell Lazarre <jlaza020@fiu.edu>
2019-10-18 23:52:28 -0400

    test: bootstrap

---
+++ src/bootstrap.spec.ts
@@ -0,0 +1,15 @@
+import { suite, test } from "mocha-typescript";
+import { env } from "process";
+import { bootstrap } from "./bootstrap";
+
+@suite
+export class BoostrapSmokeTests {
+
+    @test
+    async testBootstrap() {
+        env["BOILER_PATH"] = __dirname;
+        env["PROJECT_PATH"] = __dirname;
+        await bootstrap(["", "", "path"]);
+    }
+
+}
